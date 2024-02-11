a5954b98ec38e6a7cd9d3cd38a8a2920a86322c0
marcobrasci1986 <marco_brasci@hotmail.com>
2024-02-11 14:15:44 +0100

    Working version

--- apps/angular-app/src/app/app.module.ts
+++ apps/angular-app/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    RouterModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
